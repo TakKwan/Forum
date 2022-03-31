122afd9daf7c854e007785a69172e4d50d5e6ee4
TakKwan <waslf3651@gmail.com>
2022-03-31 17:06:35 -0400

    added useEffect and NewPost

--- client/src/components/Forum.js
+++ client/src/components/Forum.js
@@ -2,32 +2,47 @@ import { useEffect, useState } from 'react'
 import Post from './Post'
 import SearchBar from './SearchBar'
 import axios from 'axios'
+import { backend } from '../resources/resources'
+import NewPost from './NewPost'
 
-const Forum = () => {
+const Forum = ({ userId }) => {
   const [posts, setPosts] = useState([])
 
   useEffect(async () => {
-    const result = await getPosts()
-    setPosts(result.data)
-  }, [])
+    if (!posts.length) {
+      const result = await getPosts()
+      if (result.data.length) setPosts(result.data)
+    }
+  }, [posts])
 
   const getPosts = () => {
-    return axios.get('http://localhost:3001/posts')
+    return axios.get(backend.baseUrl + backend.post.readAll)
   }
 
-  const onSearch = async (searchTerms) => {
-    const result = await axios.get('http://localhost:3001/getposts', {
-      params: { searchTerms }
-    })
-    setPosts(result.data)
+  const setPost = (post) => {
+    setPosts([...posts, post])
+  }
+
+  const removePost = (postToRemove) => {
+    setPosts(
+      posts.filter(
+        (post) => post._id !== postToRemove._id || userId !== postToRemove.user
+      )
+    )
   }
 
   return (
     <div className="forum">
-      <SearchBar onSubmit={onSearch} />
+      <SearchBar onSubmit={setPosts} />
       {posts.map((post) => (
-        <Post key={post._id} {...post} />
+        <Post
+          key={post._id}
+          {...post}
+          disabled={post.user._id !== userId}
+          removePost={removePost}
+        />
       ))}
+      {userId && <NewPost userId={userId} setPost={setPost} />}
     </div>
   )
 }
